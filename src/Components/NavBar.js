b11cfbdc577b357a2fe981e9b85d0a46184eea9c
SHATHISH <satheesh12072004@gmail.com>
2024-09-19 06:30:44 +0530

    Added image folder and added navigation menu for the website

---
+++ src/Components/NavBar.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+      <div className="container">
+        <a href="#" className="navbar-brand">
+          Frontend BootCamp
+        </a>
+
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navMenu"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navMenu">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a href="#learn" className="nav-link">
+                What You'll Learn
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#questions" className="nav-link">
+                Questions
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#instructors" className="nav-link">
+                Instructors
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
